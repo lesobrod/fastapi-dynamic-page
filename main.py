06ba4d378b5bbb4efb7b39501dd8d887ae41b2d1
Denis <lesobrod@yandex.ru>
2022-04-22 08:55:10 +0300

    1.4

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = FastAPI(title="List API")
 with open('template.html', 'r', encoding='UTF-8') as file:
     HTML = file.read()
 
+
 @app.get("/")
 async def get():
     return HTMLResponse(HTML)
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
